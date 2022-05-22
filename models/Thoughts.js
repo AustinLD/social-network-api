199210fc5ea0a90871059d44fff5500ed1775377
Austin Donovan <AustinLDonovan@gmail.com>
2022-05-21 23:33:36 -0400

    date format from video copied but untested

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -5,11 +5,24 @@ const dateFormat = require("../utils/dateFormat");
 const thoughtSchema = new Schema(
   {
     thoughtPost: {
-
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 500
     },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Reaction",
+        }
+      ],
     createdAt: {
       get: timestamp => dateFormat(timestamp)
-    }
+    },
   },
   {
     toJSON: {
